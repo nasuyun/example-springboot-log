3e6f8ebbd310a1ca38c5f72c7bd55c63575147d6
HuaB Ma <5532577@qq.com>
2023-02-25 21:20:05 +0800

    log4j for serverless

--- src/main/java/com/nasuyun/example/springbootlog/ScheduledLog.java
+++ src/main/java/com/nasuyun/example/springbootlog/ScheduledLog.java
@@ -13,7 +13,7 @@ public class ScheduledLog {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 3000)
     public void reportCurrentTime() {
         log.info("The time is now {}", dateFormat.format(new Date()));
     }
